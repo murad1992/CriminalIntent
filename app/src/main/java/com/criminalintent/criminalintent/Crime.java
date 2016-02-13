a6b1416d0331795cace5ac055a57e3bc5d511c59
Murad Muradov <murkagam@mail.ru>
2016-02-13 18:16:10 +0300

    Настройка виджета EditText

--- app/src/main/java/com/criminalintent/criminalintent/Crime.java
+++ app/src/main/java/com/criminalintent/criminalintent/Crime.java
@@ -4,14 +4,15 @@ package com.criminalintent.criminalintent;
 import java.util.UUID;
 
 public class Crime {
+//Поля
     private UUID mId;
-    public UUID getId() {return mId;}
-
-
     private String mTitle;
+
+//Геттеры и сеттеры
+    public UUID getId() {return mId;}
     public String getTitle() {return mTitle;}
     public void setTitle(String title) {mTitle = title;}
-
+//Конструктор
     public Crime(){
 //        Генерирование уникального идентификатора
         mId = UUID.randomUUID();
